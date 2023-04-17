54ebe0c1fc2a4df24e0172b46e45a36341aca7fc
Todsavad Tangtortan <guntsvzz@gmail.com>
2023-04-17 23:41:17 +0700

    update

---
+++ Calucation_electricity.py
@@ -0,0 +1,40 @@
+def calculate_electricity_charge(monthly_usage, ft_rate):
+    # Define the base tariff rates in Baht/kWh
+    tariff_0_15 = 2.3483
+    tariff_16_25 = 2.9882
+    tariff_26_35 = 3.2405
+    tariff_36_100 = 3.6237
+    tariff_101_150 = 3.7171
+    tariff_151_400 = 4.2218
+    tariff_above_400 = 4.4217
+
+    # Define the tax rate as a fraction
+    tax_rate = 0.07
+    
+    # Calculate the base tariff charge
+    if monthly_usage <= 15:
+        base_tariff = monthly_usage * tariff_0_15
+    elif monthly_usage <= 25:
+        base_tariff = 15 * tariff_0_15 + (monthly_usage - 15) * tariff_16_25
+    elif monthly_usage <= 35:
+        base_tariff = 15 * tariff_0_15 + 10 * tariff_16_25 + (monthly_usage - 25) * tariff_26_35
+    elif monthly_usage <= 100:
+        base_tariff = 15 * tariff_0_15 + 10 * tariff_16_25 + 10 * tariff_26_35 + (monthly_usage - 35) * tariff_36_100
+    elif monthly_usage <= 150:
+        base_tariff = 15 * tariff_0_15 + 10 * tariff_16_25 + 10 * tariff_26_35 + 65 * tariff_36_100 + (monthly_usage - 100) * tariff_101_150
+    elif monthly_usage <= 400:
+        base_tariff = 15 * tariff_0_15 + 10 * tariff_16_25 + 10 * tariff_26_35 + 65 * tariff_36_100 + 50 * tariff_101_150 + (monthly_usage - 150) * tariff_151_400
+    else:
+        base_tariff = 15 * tariff_0_15 + 10 * tariff_16_25 + 10 * tariff_26_35 + 65 * tariff_36_100 + 50 * tariff_101_150 + 250 * tariff_151_400 + (monthly_usage - 400) * tariff_above_400
+        
+    # Calculate the Ft charge
+    ft_charge = monthly_usage * ft_rate
+    
+    # Calculate the tax
+    tax = (base_tariff + ft_charge) * tax_rate
+    
+    # Calculate the total bill
+    total_bill = base_tariff + ft_charge + tax
+    
+    return total_bill
+

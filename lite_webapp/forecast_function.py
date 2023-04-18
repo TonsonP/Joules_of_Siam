ace811fe911ac989962db62ce99019c7e4069fff
= <=>
2023-04-18 09:37:03 +0700

    update lite version

---
+++ lite_webapp/forecast_function.py
@@ -0,0 +1,192 @@
+from xgboost import XGBRegressor
+from sklearn.neural_network import MLPRegressor
+
+import numpy as np
+import pandas as pd
+import pickle
+
+class forecasting():
+
+    def __init__(self, 
+                 GDP_percent=2, 
+                 Population_percent=0.05, 
+                 CPI_percent=2,
+                 Start_date = '2023-01',
+                 End_date = '2033-01'
+                 ):
+        
+        # Check date eligibility
+        self.Check_date_eligibility(Start_date, End_date)
+
+        # Get Static temperature values
+        file = open("./dataP/forecast_temperature.pkl", "rb")
+        Temperature_forecast = pickle.load(file)
+        self.Temperature_forecast = Temperature_forecast[0:len(self.Date)]
+
+        # Get forecasted LSTM values
+        LSTM_forecast = pickle.load(open("./dataP/forecast_LSTM.pkl", "rb"))
+        self.LSTM_forecast = LSTM_forecast[0:len(self.Date)]
+
+        # Get forecasted ARIMA values
+        ARIMA_forecast = pickle.load(open("./dataP/forecast_ARIMA.pkl", "rb"))
+        self.ARIMA_forecast = ARIMA_forecast[0:len(self.Date)]
+
+        # Get forecasted SARIMA values
+        SARIMA_forecast = pd.read_csv("./dataP/SARIMA_forecasts.csv")["Peak Forecast"]
+        self.SARIMA_forecast = SARIMA_forecast[0:len(self.Date)]
+
+
+        # Initial values for generating features
+        self.GDP_value = 924033.33
+        self.Population_value = 66.09
+        self.CPI_value = 106.47
+
+        # Initial percentages
+        self.GDP_percent = GDP_percent
+        self.Population_percent = Population_percent
+        self.CPI_percent = CPI_percent
+
+        # Initial XGBoost model
+        self.XGBoost_model = XGBRegressor()
+        self.XGBoost_model.load_model("./dataP/XGBoost_model.json")
+
+        # # Initial ANN model
+        # self.ANN_scaler = pickle.load(open('./Data/monthly_scaler.pkl', 'rb'))
+        # self.ANN_model = pickle.load(open('./Data/ANN_monthly.pkl', 'rb'))
+
+        # Prepare data
+        self.prepare_data()
+
+        # Prediction
+        self.get_prediction()
+
+        # Ploting Data
+        self.plotting_data()
+    
+    def Check_date_eligibility(self, start_date, end_date):
+        
+        start_date = np.array(start_date, dtype='datetime64[M]')
+        end_date   = np.array(end_date, dtype='datetime64[M]')
+
+        date_delta = end_date - start_date
+
+        if date_delta.astype(int) < 0:
+            raise ValueError('Invalid start_date/end_date')
+        else:
+            self.Start_date = start_date
+            self.End_date   = end_date
+            Date        = np.arange(start_date, end_date, dtype='datetime64[M]')
+            self.months = Date.astype('datetime64[M]').astype(int) % 12 + 1
+            self.years  = Date.astype('datetime64[Y]').astype(int) + 1970
+            self.Date   = Date.astype(str).tolist()
+
+    # Function to convert integer to list.
+    def as_list(self, x):
+        if type(x) is list:
+            return x
+        else:
+            return [x]
+    
+
+    # Function to generated prediction.
+    def assume_predict(self, value, percent_list):
+        
+        results = list()
+        temp = list()
+        temp_val = value
+
+        percent_list = self.as_list(percent_list)
+
+        for percent in percent_list:
+
+            # Divided % per years to per months
+
+            percent_month = percent/12
+
+            for current_date in self.Date:
+                temp_val = temp_val + (temp_val * percent_month/100)
+                temp.append(temp_val)
+
+            temp_val = value
+            results.append(temp)
+            temp = list()
+        return results[0]
+    
+    def prepare_data(self):
+
+        # Get Assume values
+        GDP_forecast = self.assume_predict(self.GDP_value, self.GDP_percent)
+        Population_forecast = self.assume_predict(self.Population_value, self.Population_percent)
+        CPI_forecast = self.assume_predict(self.CPI_value, self.CPI_percent)
+
+        # Create DataFrame/features for XGBoost
+        features_dataframe = pd.DataFrame([self.Date, self.years, self.months, 
+                                           Population_forecast, self.Temperature_forecast, 
+                                            CPI_forecast, GDP_forecast]).T
+        features_dataframe.columns = ["Date", "Year", "Month", "Population", "Temperature", 
+                                    "CPI", "GDP"]
+        
+        # Convert DataFrame types
+        features_dataframe['Date']= pd.to_datetime(features_dataframe['Date'])
+        features_dataframe['Year']= features_dataframe['Date'].dt.year
+        features_dataframe['Month']= features_dataframe['Date'].dt.month
+        
+        features_dataframe[["Population", "Temperature", "CPI", "GDP"]] = \
+        features_dataframe[["Population", "Temperature", "CPI", "GDP"]].astype(float)
+
+        
+        features_dataframe.set_index("Date", inplace=True)
+        
+        self.XGBoost_features = features_dataframe
+
+        # Create features for ANN
+        self.ANN_features = features_dataframe[["Population", "Temperature", "CPI", "GDP"]].values
+
+    def get_prediction(self):
+        
+        # XG_Boost prediction
+        self.XGBoost_prediction = self.XGBoost_model.predict(self.XGBoost_features)
+        
+        # ANN prediction
+        #self.ANN_prediction     = self.ANN_model.predict(self.ANN_scaler.transform(self.ANN_features))
+
+    def plotting_data(self):
+
+        # With ANN
+        # self.plotting_value = pd.DataFrame([self.Date, self.LSTM_forecast, 
+        #                                     self.XGBoost_prediction, self.ANN_prediction, 
+        #                                     self.ARIMA_forecast, self.SARIMA_forecast]).T
+        
+        self.plotting_value = pd.DataFrame([self.Date, self.LSTM_forecast, 
+                                    self.XGBoost_prediction, self.ARIMA_forecast, 
+                                    self.SARIMA_forecast]).T
+        
+
+
+        # self.energy_reserve_20 = 42909.9#self.LSTM_forecast.mean() * 1.20
+        # self.energy_reserve_35 = 49144.8#self.LSTM_forecast.mean() * 1.35
+        # self.energy_reserve_50 = self.LSTM_forecast.mean() * 1.50
+        self.plotting_value.columns = ["Date", "LSTM", "XGBoost", "ARIMA", "SARIMA"]
+        #self.plotting_value["ANN"] = self.plotting_value["ANN"].abs()
+
+        # Convert Datatype
+        #self.plotting_value["Date"] = pd.to_datetime(df['Date'])
+        self.plotting_value[["LSTM", "XGBoost", "ARIMA", "SARIMA"]] = \
+        self.plotting_value[["LSTM", "XGBoost", "ARIMA", "SARIMA"]].astype(float)
+
+        # Insert current maximum capacity.
+        self.plotting_value.insert(loc=len(self.plotting_value.columns), 
+                                   column="Maximum_capacity",
+                                   value= 49144.80
+                                   )
+        
+        self.plotting_value.insert(loc=len(self.plotting_value.columns), 
+                                   column="Maximum_capacity_wo_import",
+                                   value= 42909.9
+                                   )
+
+if __name__ == '__main__':
+    print('hi')
+    forecast = forecasting(GDP_percent=2, Population_percent=0.05, CPI_percent=2)
+    forecast.plotting_value.to_csv('dataP/lightForcast.csv')
+    # pass
---
+++ lite_webapp/main.py
@@ -0,0 +1,288 @@
+import dash
+from dash import html, dcc, Output, Input, State
+import flask
+import plotly.express as px
+import pandas as pd
+import pickle
+from flask import Flask, render_template, redirect, url_for, flash, request, send_from_directory
+# from flask_bootstrap import Bootstrap
+import plotly.graph_objects as go
+# from forecast_function import forecasting
+from plotly.subplots import make_subplots
+from Calucation_electricity import calculate_electricity_charge
+
+# from flask_ckeditor import CKEditor
+# import flask
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.serving import run_simple
+
+# Server and Dashboard web app
+server = flask.Flask(__name__)
+app1 = dash.Dash(requests_pathname_prefix="/app1/")
+app2 = dash.Dash(requests_pathname_prefix="/app2/")
+
+# Data for web load preparing
+with open('dataP/Actual_values.pkl', 'rb') as file:
+    actial_values = pickle.load(file)
+
+with open('dataP/df_generation_type_2022.pkl', 'rb') as file:
+    df_generation_type_2022 = pickle.load(file)
+
+with open('dataP/df_generation_sector_2022.pkl', 'rb') as file:
+    df_consumption_2022 = pickle.load(file)
+
+with open('dataP/df_consumption_month_2022.pkl', 'rb') as file:
+    df_month_2022 = pickle.load(file)
+
+FT_prediction = pd.read_csv("dataP/FT_full.csv")
+FT_prediction['Date'] = pd.to_datetime(FT_prediction['Date'])
+FT_prediction.set_index("Date", inplace=True)
+forecast = pd.read_csv('dataP/lightForcast.csv')
+# check
+# This still temp
+# test2 = pd.read_csv('dataP/allmodelpredictedsaved.csv')
+
+
+# Figure for dashboard
+figsubpie = make_subplots(rows=1, cols=2, subplot_titles=('Electricity generation Group by type', 'Electricity  consumption Group by sector'), specs=[
+                          [{'type': 'pie'}, {'type': 'pie'}]])
+
+# Electricity generation Group by type
+labels = df_generation_type_2022.index[0:7]
+values = df_generation_type_2022[0:7]
+
+pietype_generation_fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+pietype_generation_fig.update_layout(title=dict(text='Electricity generation Group by type', font=dict(size=20), automargin=False, yref='paper')
+                                     )
+figsubpie.add_trace(go.Pie(labels=labels, values=values, showlegend=True,
+                    name='Electricity generation Group by type', hole=0.7), row=1, col=1)
+# Electricity consumption Group by sector
+labels = df_consumption_2022.index[2:7]
+values = df_consumption_2022[2:7]
+consumption_sector = go.Figure(data=[go.Pie(labels=labels, values=values)])
+consumption_sector.update_layout(
+    title=dict(text="Electricity consumption Group by sector",
+               font=dict(size=20), automargin=False, yref='paper')
+)
+figsubpie.add_trace(go.Pie(labels=labels, values=values, showlegend=True,
+                    name='Electricity consumption Group by sector', hole=0.7), row=1, col=2)
+
+fig_month = px.bar(df_month_2022[df_month_2022["Year"] == 2022], x='Month', y='Grand Total',
+                   hover_data=df_month_2022.columns[2:-
+                                                    1].values, color='Residential',
+                   labels={'pop': 'Consumption seperated by month'}, height=400)
+fig_month.update_layout(
+    title=dict(text="Consumption seperated by month",
+               font=dict(size=20), automargin=False, yref='paper')
+)
+
+
+# model comsumption prediction
+# forecast = forecasting(GDP_percent=2, Population_percent=0.05, CPI_percent=2)
+# fig = px.line(forecast.plotting_value, x="Date", y=forecast.plotting_value.columns[1:])
+prediction_allmodelfig = px.line(forecast, x="Date", y=forecast.columns[1:]).add_scatter(
+    x=actial_values.index.values[130:], y=actial_values["Peak"][130:].values, name='Actual', line=dict(color='#8a938b'))
+
+
+# Route of our web
+@ server.route("/")
+def home():
+    return render_template('index.html')
+    # return "Hello, Flask!"
+
+
+@ server.route("/data_")
+def data_():
+    return render_template('left-sidebar.html')
+    # return "Hello, Flask!"
+
+
+@ server.route("/analysis_")
+def analysis_():
+    return render_template('right-sidebar.html')
+    # return "Hello, Flask!"
+
+
+@ server.route("/aboutus")
+def aboutus():
+    return render_template('no-sidebar.html')
+    # return "Hello, Flask!"
+
+
+@ server.route('/render_dashboard')
+def render_dashboard():
+    return flask.redirect('/app1')
+
+
+@ server.route('/render_dashboard2')
+def render_dashboard2():
+    return flask.redirect('/app2')
+
+
+# DashApp Layout
+app1.layout = html.Div(
+    id="app-container",
+    children=[
+        # Banner
+        html.Div(
+            id="banner",
+            className="banner",
+            children=[
+                html.Img(src=app1.get_asset_url("JouleofSiam_logo.png"))],
+        ),
+        # Left column
+
+        html.Div(
+            id="left-column",
+            className="eight columns",
+            children=[
+                # test prediction
+                html.Div(
+                    id="power predict",
+                    children=[
+                        html.B("Energy Prediction"),
+                        html.Hr(),
+                        # html.P('GDP_input_value'),
+                        # dcc.Input(id="GDP_input_value", type="number",
+                        #           placeholder="GDP growth in percentage", value=2),
+                        # html.P('Population_input_value'),
+                        # dcc.Input(id="Population_input_value", type="number",
+                        #           placeholder="Population growth in percentage", value=0.05),
+                        # html.P('CPI_input_value'),
+                        # dcc.Input(id="CPI_input_value", type="number",
+                        #           placeholder="CPI growth in percentage", value=2),
+                        dcc.Graph(figure=prediction_allmodelfig,
+                                  id="forcastmultimd", responsive=True),
+                    ],
+                ),
+                # Patient Wait time by Department
+                # html.Div(
+                #     id="wait_time_card",
+                #     children=[
+                #         html.B("Patient Wait Time and Satisfactory Scores"),
+                #         html.Hr(),
+                #         html.Div(id="wait_time_table", children=initialize_table()),
+                #     ],
+                # ),
+            ],
+        ),
+
+        # Right column
+        html.Div(
+            id="right-column",
+            className="four columns",
+            children=[html.B('Electricity generation & consumption segmentation'), html.Hr(),
+                      # dcc.Graph(figure=pietype_generation_fig),
+                      # dcc.Graph(figure=consumption_sector),
+                      dcc.Graph(figure=figsubpie),
+
+                    #   dcc.Dropdown(id='fig_month_dropdown',
+                    #                options=[
+                    #                    {'label': 'Residential',
+                    #                        'value': 'Residential'},
+                    #                    {'label': 'Business',
+                    #                     'value': 'Business'},
+                    #                    {'label': 'Industrial',
+                    #                     'value': 'Industrial'},
+                    #                    {'label': 'Government',
+                    #                     'value': 'Government and Non-Profit'},
+                    #                    {'label': 'Other sector',
+                    #                        'value': 'Other sector'}
+                    #                ],
+                    #                value='Residential'),
+                    #   html.Button('Submit', id='submit-val', n_clicks=0),
+                      # dcc.Graph(figure=fig_month)]
+                      dcc.Graph(figure=fig_month, id='fig_month_id')]
+            + [html.Div(["initial child"], id="output-clientside",
+                        style={"display": "none"})],
+        )
+    ],
+)
+
+
+# @app1.callback(Output(component_id='fig_month_id', component_property='figure'),
+#                Input(component_id='submit-val', component_property='n_clicks'),
+#                State(component_id='fig_month_dropdown', component_property='value'))
+# def Update_month_by_sector(n_clicks, fig_month_dropdown):
+
+#     print('Fig dropdown values')
+#     print(fig_month_dropdown)
+
+#     fig_month = px.bar(df_month_2022[df_month_2022["Year"] == 2022], x='Month', y='Grand Total',
+#                        hover_data=df_month_2022.columns[2:-
+#                                                         1].values, color=fig_month_dropdown,
+#                        labels={'pop': 'Consumption seperated by month'}, height=400)
+#     fig_month.update_layout(
+#         title=dict(text="Consumption seperated by month",
+#                    font=dict(size=20), automargin=False, yref='paper')
+#     )
+
+#     return fig_month
+
+
+# @app1.callback(Output(component_id='forcastmultimd', component_property='figure'),
+#                [Input(component_id='GDP_input_value', component_property='value'),
+#                Input(component_id='CPI_input_value',
+#                      component_property='value'),
+#                Input(component_id='Population_input_value', component_property='value')]
+#                )
+# def Update_forecast(GDP_input_value, CPI_input_value, Population_input_value):
+
+#     # model comsumption prediction
+#     forecast = forecasting(GDP_percent=GDP_input_value,
+#                            Population_percent=Population_input_value, CPI_percent=CPI_input_value)
+#     # fig = px.line(forecast.plotting_value, x="Date", y=forecast.plotting_value.columns[1:])
+#     prediction_allmodelfig = px.line(
+#         forecast.plotting_value, x="Date", y=forecast.plotting_value.columns[1:])
+#     prediction_allmodelfig.add_scatter(
+#         x=actial_values.index.values[130:], y=actial_values["Peak"][130:].values, name='Actual', line=dict(color='#8a938b'))
+
+#     return prediction_allmodelfig
+
+
+# app2.layout = html.Div("Hello, Dash app 2!")
+app2.layout = html.Div(
+    id="app-container",
+    children=[
+        # Banner
+        html.Div(
+            id="banner",
+            className="banner",
+            children=[
+                html.Img(src=app2.get_asset_url("JouleofSiam_logo.png"))],
+        ),
+        # Left column
+
+        html.Div(
+            id="left-column",
+            className="eight columns",
+            children=[
+                # test prediction
+                html.Div(
+                    id="FT predict",
+                    children=[
+                        html.B("FT Prediction"),
+                        html.Hr(),
+                        dcc.Graph(figure=px.line(FT_prediction, x=FT_prediction.index.values[0:], y=FT_prediction["FT"].iloc[0:]),
+                                  id="FT_predict", responsive=True),
+                    ],
+                ),
+
+            ],
+        ),
+        # + [html.Div(["initial child"], id="output-clientside",
+        #             style={"display": "none"})],
+
+    ],
+)
+
+
+application = DispatcherMiddleware(
+    server,
+    {"/app1": app1.server, "/app2": app2.server},
+)
+
+if __name__ == "__main__":
+    run_simple("localhost", 8051, application,
+               use_reloader=True, use_debugger=True)
+    #    app.run_server(debug=True)
